4ae44d3bf6c50b95f9fcfde2a13295ca70003a8d
Hypermicro-code <tony2mat@gmail.com>
2025-05-09 10:52:24 +0200

    Create firebase-init.js

---
+++ firebase-init.js
@@ -0,0 +1,16 @@
+// firebase-init.js
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDVCiUp3qy0hYLQQ3D1jPpfndCVOKEHEZg",
+  authDomain: "befaringsappen.firebaseapp.com",
+  projectId: "befaringsappen",
+  storageBucket: "befaringsappen.appspot.com",
+  messagingSenderId: "455917300957",
+  appId: "1:455917300957:web:99c0259444c7a7ac5f7f34",
+  measurementId: "G-T8E9CXZNJ4",
+  databaseURL: "https://befaringsappen-default-rtdb.europe-west1.firebasedatabase.app/"
+};
+
+// Initialiser app og database
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
